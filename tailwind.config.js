547c58d84db57460ed7a725471a30d61abf08032
mior <miorpunye@gmail.com>
2021-12-17 15:48:00 +0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{tsx}','./src/**/*.{tsx,jsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
