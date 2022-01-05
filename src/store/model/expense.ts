14cacd7259a3cba2ae813865f3cfc8ddfaa11912
mior <miorpunye@gmail.com>
2022-01-05 17:29:33 +0800

    feat: refactor expense store

--- src/store/model/expense.ts
+++ src/store/model/expense.ts
@@ -1,6 +1,11 @@
-import { Action, action, persist, thunk, Thunk } from "easy-peasy";
+import { Action, action, persist, Thunk, thunk } from "easy-peasy";
 import { IStoreModel } from "../index";
-import { IGetGroup, IGroup } from "./group";
+
+interface IPaymentLogs {
+  user: string;
+  amount: number;
+  created: string; // ISO string date
+}
 
 export interface IExpenseValue {
   id?: string;
@@ -8,23 +13,28 @@ export interface IExpenseValue {
   users: string[]; // user.id
   total: number;
   groupId: string;
+  created: string; // ISO string date
+  payment: IPaymentLogs[];
 }
 
-export interface IGetExpenseByGroup extends Omit<IExpenseValue, "groupId"> {}
+export interface IGetExpensesByGroup extends Omit<IExpenseValue, "groupId"> {}
 
 export interface IExpense {
-  [groupId: string]: IGetExpenseByGroup[];
+  [groupId: string]: IGetExpensesByGroup[];
 }
+
+export interface ISetExpenses
+  extends Omit<IExpenseValue, "created" | "payment"> {}
 
 export interface IExpenseModel {
   expenses: IExpense;
-  setExpenses: Action<IExpenseModel, IExpenseValue>;
+  setExpenses: Action<IExpenseModel, ISetExpenses>;
   getExpenseByGroup: Thunk<
     IExpenseModel,
     string,
     undefined,
     IStoreModel,
-    IGetExpenseByGroup[]
+    IGetExpensesByGroup[]
   >;
 }
 
@@ -33,7 +43,17 @@ export const expenseModel: IExpenseModel = persist(
     expenses: {},
     setExpenses: action((state, { groupId, ...expense }) => {
       expense["id"] = `expense-${new Date().getTime()}`;
-      state.expenses[groupId].push(expense);
+
+      // initialize new expenses group
+      if (!state.expenses[groupId]) {
+        state.expenses[groupId] = [];
+      }
+
+      state.expenses[groupId].push({
+        ...expense,
+        created: new Date().toISOString(),
+        payment: [],
+      });
     }),
     getExpenseByGroup: thunk((actions, groupId, helpers) => {
       const expenseByGroup = helpers.getState().expenses?.[groupId];
