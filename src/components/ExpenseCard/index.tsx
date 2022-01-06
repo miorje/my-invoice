ba9c764cd89ebbedf29dc953813d1ee83d6b41bb
mior <miorpunye@gmail.com>
2022-01-06 16:58:52 +0800

    feat: expense card format date

--- src/components/ExpenseCard/index.tsx
+++ src/components/ExpenseCard/index.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent, useMemo } from "react";
 import { IGetExpensesByGroup } from "../../store/model/expense";
+import exp from "constants";
+import dayjs from "dayjs";
 
 export const ExpenseCard: FunctionComponent<IGetExpensesByGroup> = (
   expense
@@ -12,14 +14,25 @@ export const ExpenseCard: FunctionComponent<IGetExpensesByGroup> = (
       }).format(expense.total),
     [expense.total]
   );
+
+  const date = useMemo(
+    () => ({
+      month: dayjs(expense.created).format("MMM"),
+      day: dayjs(expense.created).format("DD"),
+    }),
+    [expense.created]
+  );
   return (
     <div
       key={expense.id}
-      className="grid grid-cols-3 gap-4 border-gray-700 border-2 rounded-lg shadow-md"
+      className="grid grid-cols-12 gap-4 border-gray-700 rounded-lg shadow-md"
     >
-      <div className="col">{expense.created}</div>
-      <div className="col col-span-8">{expense.name}</div>
-      <div className="col">{total}</div>
+      <section className="col col-span-1">
+        <h2 className="text-xl font-extrabold text-gray-800">{date.month}</h2>
+        <span>{date.day}</span>
+      </section>
+      <div className="col col-start-2 col-end-11">{expense.name}</div>
+      <div className="col col-span-1 col-end-auto ">{total}</div>
     </div>
   );
 };
