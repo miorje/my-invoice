aef29e932f000fce7831e02ee35efd1b7b7d01bb
mior <miorpunye@gmail.com>
2022-01-12 11:11:59 +0800

    feat: group card layour

--- src/components/UserAvatar/index.tsx
+++ src/components/UserAvatar/index.tsx
@@ -1,14 +1,14 @@
 import { Avatar } from "./Avatar";
 
-interface IUserAvatarGroup{
-  users:string[];
+interface IUserAvatarGroup {
+  users: string[];
 }
 
 export const UserAvatarGroup = (props: IUserAvatarGroup) => {
   return (
-    <div className=" flex mb-2">
-      {props.users.map((user,index) => (
-        <Avatar id={user} key={user} index={index}/>
+    <div className=" flex">
+      {props.users.map((user, index) => (
+        <Avatar id={user} key={user} index={index} />
       ))}
     </div>
   );
