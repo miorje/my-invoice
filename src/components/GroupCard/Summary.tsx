0ae24005e8fcf8ffb01a651992bdf5b560bd079e
mior <miorpunye@gmail.com>
2022-01-12 12:51:43 +0800

    feat: add total paid in group card

--- src/components/GroupCard/Summary.tsx
+++ src/components/GroupCard/Summary.tsx
@@ -1,18 +1,60 @@
 import { IGroup } from "../../store/model/group";
 import { UserAvatarGroup } from "../UserAvatar";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
+import { useStoreState } from "../../store/hooks";
+import { useMemo } from "react";
+import { TotalSummary } from "./TotalSummary";
 
 export const Summary = (group: IGroup) => {
   const router = useRouter();
 
   const handleToGroup = async () => {
-    await router.push(`/group/${group.id}`)
-  }
+    await router.push(`/group/${group.id}`);
+  };
 
+  const expenseById = useStoreState((state) =>
+    state.expense.expenseByGroupId(group.id as string)
+  );
+
+  const totalExpenses = useMemo(() => {
+    return expenseById?.reduce((total, expense) => total + expense.total, 0);
+  }, [expenseById]);
+
+  const totalPaid = useMemo(
+    () =>
+      expenseById.reduce(
+        (total, expense) =>
+          total +
+          expense.payment.reduce(
+            (totalPayment, pay) => totalPayment + pay.total,
+            0
+          ),
+        0
+      ),
+    [expenseById]
+  );
+
+  console.log(totalPaid);
   return (
-    <div onClick={handleToGroup} className="border-2 border-black pt-1 pb-3 px-1 cursor-pointer">
-      <h2>{group.name}</h2>
-      <UserAvatarGroup users={group.users} />
+    <div
+      onClick={handleToGroup}
+      className="shadow-md border-t-2 border-gray-100 rounded-lg rounded-tr-2xl rounded-bl-2xl pt-3 pb-5 px-1 cursor-pointer"
+    >
+      <section
+        className="text-center flex justify-center flex-col"
+        aria-describedby="group=header"
+      >
+        <div className="flex justify-center mt-2">
+          <UserAvatarGroup users={group.users} />
+        </div>
+        <h2 className="font-medium text-gray-800 text-lg mt-0.5">
+          {group.name}
+        </h2>
+      </section>
+      <div className="grid grid-cols-2 mt-2">
+        <TotalSummary header={"Paid"} total={totalPaid} />
+        <TotalSummary header={"Total"} total={totalExpenses} />
+      </div>
     </div>
   );
 };
